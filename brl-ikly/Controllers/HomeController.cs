2c58fc4870efa0edd47acc09594d4e481ccc7924
Peso De Guzman <ajvdeguzman@hotmail.com>
2017-05-05 18:50:32 +0800

    Rewrote about, contact

--- brl-ikly/Controllers/HomeController.cs
+++ brl-ikly/Controllers/HomeController.cs
@@ -16,15 +16,11 @@ namespace brl_ikly.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
