48353ec72c7216095ca6d79220571f20644c9055
Peso De Guzman <ajvdeguzman@hotmail.com>
2017-05-05 11:02:27 +0800

    Add project files.

---
+++ brl-ikly/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace brl_ikly
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ brl-ikly/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="brl_ikly.MvcApplication" Language="C#" %>
