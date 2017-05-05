ac82ad053ab3df9a43dd613a533c27f6df9a01b0
Peso De Guzman <ajvdeguzman@hotmail.com>
2017-05-05 12:37:22 +0800

    Added DB configurations and Url model

---
+++ brl-ikly/Models/Url.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace brl_ikly.Models
+{
+    public class Url
+    {
+        public int UrlId { get; set; }
+        public string UrlLongName { get; set; }
+        public string UrlShortName { get; set; }
+        public int UrlVisitCount { get; set; }
+    }
+
+    public class UrlDBContext : DbContext
+    {
+        public DbSet<Url> Urls { get; set; }
+    }
+}
